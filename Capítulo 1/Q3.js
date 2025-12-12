d7b2525f3293dc0d45d9983570e7e208e9341926
Emily-oha <ohanasoares637@gmail.com>
2025-12-12 11:05:00 -0300

    Capítulo 2

--- Capítulo 1/Q3.js
+++
@@ -1,7 +0,0 @@
-//3. // Calcular a média de três números
-let n1 = parseFloat(prompt("Digite o primeiro número:"));
-let n2 = parseFloat(prompt("Digite o segundo número:"));
-let n3 = parseFloat(prompt("Digite o terceiro número:"));
-
-let media = (n1 + n2 + n3) / 3;
-alert("A média é: " + media.toFixed(2));
